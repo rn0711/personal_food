28d04d9a580bb3fb0bbfc4c4c75ede9e95029fda
rn0711 <rena_255@icloud.com>
2022-03-22 15:26:30 +0900

    パスワード英数混合、全角カナの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,      presence: true
-  validates :first_name,    presence: true
-  validates :last_name,     presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'に全角文字を使用してください' } do
+  validates :first_name
+  validates :last_name
+  end
   validates :date_of_birth, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end
