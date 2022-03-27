c0f8c8624cfe0ef72827a98a95992a6d17cee8f7
rn0711 <rena_255@icloud.com>
2022-03-27 18:24:59 +0900

    ActiveHashの導入

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   has_one_attached :image
+
+  belongs_to :category
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
 end
