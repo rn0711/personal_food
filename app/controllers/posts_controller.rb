a8bcd3db8b86be7b4a5475c77e130f53079f0955
rn0711 <rena_255@icloud.com>
2022-03-27 18:18:48 +0900

    Active Storageの導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order("created_at DESC")
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image).merge(user_id: current_user.id)
+  end
 end
