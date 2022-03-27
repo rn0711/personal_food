3d12b13038bfbc6f107c7c4b6709a4a33f8a48dd
rn0711 <rena_255@icloud.com>
2022-03-27 18:28:11 +0900

    newアクションのコントローラー定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,13 @@ class PostsController < ApplicationController
     @posts = Post.all.order("created_at DESC")
   end
 
+  def new
+    @post = Post.new
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:image).merge(user_id: current_user.id)
+    params.require(:post).permit(:image, :title, :category_id, :text).merge(user_id: current_user.id)
   end
 end
