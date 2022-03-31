77a82d46e4bb065760c3a6ca78836c97212b4f7a
rn0711 <rena_255@icloud.com>
2022-03-31 12:31:00 +0900

    createアクションのコントローラー定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,21 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @posts = Post.all.order("created_at DESC")
   end
 
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   private
