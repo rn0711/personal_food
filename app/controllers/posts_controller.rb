454ffc42888b3f66cc6855366f06ef60a33873d4
rn0711 <rena_255@icloud.com>
2022-04-01 17:16:34 +0900

    編集画面への遷移

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :new, :create, :show]
+  before_action :authenticate_user!, only: [:index, :new, :create, :show, :update]
   
   def index
     @posts = Post.all.order("created_at DESC")
@@ -22,6 +22,19 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+  
   private
 
   def post_params
